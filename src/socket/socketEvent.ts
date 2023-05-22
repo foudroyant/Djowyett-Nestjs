2a21ee12c8d3112132dc5a90553a2c2a94231d88
Foudroyant <stephanebazebibouta@gmail.com>
2023-05-22 05:13:29 +0100

    first commit

---
+++ src/socket/socketEvent.ts
@@ -0,0 +1,30 @@
+import {ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer} from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
+@WebSocketGateway({
+    cors:{origin:"*"}
+})
+export class SocketEvent {
+
+    @WebSocketServer()
+    server:Server
+
+    handleConnect(client:Socket){
+        console.log(`Connexion : ${client.id}`)
+    }
+
+    handleDisconnect(client:Socket){
+        console.log(`Client disconnect : ${client.id}`)
+    }
+
+    @SubscribeMessage("otp")
+    handlEvent(@MessageBody() data:string, @ConnectedSocket() client:Socket){
+        console.log(data)
+        this.server.emit("otp", client.id, data)
+    }
+
+    @SubscribeMessage("feedback")
+    handelEvent(@MessageBody() data:string, @ConnectedSocket() client:Socket){
+        console.log(data)
+        this.server.emit("feedback", client.id, data)
+    }
+}
